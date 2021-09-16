eb169ee8db88bdc4c20b6eeeb0713fc9ee64d3f9
n8feldman <nfeldman1995@gmail.com>
2021-09-15 22:52:25 -0400

    fixed client dev port

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const auth = require('./lib/auth')
 // define server and client ports
 // used for cors and local port declaration
 const serverDevPort = 4741
-const clientDevPort = 7165
+const clientDevPort = 7000
 
 // establish database connection
 // use new version of URL parser
